ab3f068c73203d10172f8c052f90df8b91f90086
Ender Calderón <177250779+Yohebyth@users.noreply.github.com>
2024-11-16 06:59:14 +0000

    generated condition

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const Home = () => {	
 
-//create your first component
-const Home = () => {
+	const [need, setNeed] = useState("");
+	const handleOnChange = (e) => {	
+		if(e.key === "Enter"){
+			if(e.target.value.trim() !== ""){
+				setNeed(e.target.value.trim());			
+			} else alert("The field cannot be empty");
+												
+		}		
+	}	
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>Prueba</h1>		
+			<input type="text" placeholder="What needs to be done? " onKeyDown={ handleOnChange} />	
+			<ul>{need}</ul>
 		</div>
 	);
 };
